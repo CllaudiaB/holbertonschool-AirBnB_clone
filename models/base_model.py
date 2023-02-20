ed1eb73d3456916fa1d47281c5494baad8b9fb40
annlaure02 <alaure.g02@gmail.com>
2023-02-20 05:57:14 -0800

    added base model- changed method to_dict and make instance attributes in def init

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,10 @@ class BaseModel:
     """ 
     define all common attributes/methods for other classes 
     """
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -31,9 +32,8 @@ class BaseModel:
         """
         Returns a dictionary containing all keys/values of __dict__ of the instance
         """
-        return {
-            "id": self.id,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
-            "__class__": __class__.__name__
-        }
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = __class__.__name__
+        new_dict["created_at"] = datetime.isoformat(self.created_at)
+        new_dict["updated_at"] = datetime.isoformat(self.updated_at)
+        return new_dict
