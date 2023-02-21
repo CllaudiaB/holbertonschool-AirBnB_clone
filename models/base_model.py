c5cea31fde241fa1e8d393ccc8524bd7c2d69d29
annlaure02 <alaure.g02@gmail.com>
2023-02-21 07:08:02 -0800

    rechanged init else created_at

--- models/base_model.py
+++ models/base_model.py
@@ -27,6 +27,7 @@ class BaseModel:
             
         else:
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
 
