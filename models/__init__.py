0572dc1c0bab0e6e002e5c8e8172267e8b46b3ea
annlaure02 <alaure.g02@gmail.com>
2023-02-21 01:29:04 -0800

    changed import

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 create a unique FileStorage instance
 """
 
-from models.engine.file_storage import file_storage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage
 storage.reload()
