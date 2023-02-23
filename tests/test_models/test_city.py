afe6a1a780edf02e6f2843cf024ffe09d28d23e8
annlaure02 <alaure.g02@gmail.com>
2023-02-23 08:46:00 -0800

    changed test_city file

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,14 +7,22 @@ from models.city import City
 
 class Test_City(unittest.TestCase):
     """ unittest for city file """
-    def test_state_id(self):
-        """ test attribute state_id """
+    def test_instance(self):
+        "Test instance"
         city = City()
-        city.state_id = ""
-        self.assertEqual(city.state_id, "")
+        self.assertIsInstance(city, City)
 
-    def test_name(self):
-        """ test attribute name """
+    def test_city_name(self):
+        "Test city name"
         city = City()
-        city.name = ""
-        self.assertEqual(city.name, "")
+        self.assertEqual("", city.name)
+
+    def test_id(self):
+        "Test id"
+        city = City()
+        self.assertEqual(str, type(city.id))
+
+    def test_state_id(self):
+        "Test state id"
+        city = City()
+        self.assertEqual("", city.state_id)
